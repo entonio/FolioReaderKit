b5b0c08499f3924d1d27185d373614339d1e388f
António Pedro Marques <entonio@gmail.com>
2025-04-25 19:28:21 +0000

    fontblaster

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/cxa/MenuItemKit.git", exact: "3.1.3"),
         .package(url: "https://github.com/entonio/ZFDragableModalTransition.git", branch: "master"),
         .package(url: "https://github.com/tadija/AEXML.git", branch: "master"),
-        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "5.3.0"),
+        .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", branch: "master"),
         .package(url: "https://github.com/realm/realm-swift.git", from: "10.54.4")
     ],
     targets: [
