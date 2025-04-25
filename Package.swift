fe20ea75128ac15acbf9950ed1f965f034cb5e7d
António Pedro Marques <entonio@gmail.com>
2025-04-25 18:54:44 +0000

    ZFDragableModalTransition

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.1.0"),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: "3.0.0"),
-        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: "0.6.0"),
+        .package(url: "https://github.com/entonio/ZFDragableModalTransition.git", branch: "master"),
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.2.0"),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "4.0.0"),
         .package(url: "https://github.com/fantim/JSQWebViewController.git", from: "6.1.0"),
